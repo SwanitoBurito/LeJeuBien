7e97e01b810d5b5fd80041a9fe6b259f34e32a07
Remi laptop <remi@uselessworks.com>
2016-05-22 17:57:13 +0200

    RaycastSpawner

---
+++ Assets/Scripts/RaycastSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastSpawner : MonoBehaviour {
+
+    public Camera sourceCamera;
+    public GameObject objectPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = sourceCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Ground")))
+            {
+                GameObject.Instantiate(objectPrefab, hit.point, Quaternion.identity);
+            }
+        }
+        
+	}
+}
