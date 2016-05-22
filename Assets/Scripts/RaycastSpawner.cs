295df9cb71713351a30eb8c0c77c9ead1ece7b83
Swann Martin-Raget <swann@swann.swann>
2016-05-22 20:28:51 +0200

    Moulte truc bien

--- Assets/Scripts/RaycastSpawner.cs
+++ Assets/Scripts/RaycastSpawner.cs
@@ -6,27 +6,50 @@ public class RaycastSpawner : MonoBehaviour {
     public Camera sourceCamera;
     public GameObject objectPrefab;
 
+    public float godSphereSeedOnY = 0.1f;
+
     protected GameObject playerGo;
     protected GameObject mouseCursor;
+
+    private float coolDownBetweenMobSpawn = 0.8f;
+    private float coolDownTimer;
 
     // Use this for initialization
     void Start ()
     {
 	    playerGo = GameObject.Find("_Player");
         mouseCursor = GameObject.Find("Cursor");
+        coolDownTimer = coolDownBetweenMobSpawn;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //Sphere du player
         Ray rayMouse = sourceCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitMouse;
         if (Physics.Raycast(rayMouse, out hitMouse, 100.0f, LayerMask.GetMask("Ground")))
         {
-            mouseCursor.transform.position = hitMouse.point;
+            Vector3 posWithoutY = new Vector3(hitMouse.point.x, mouseCursor.transform.position.y, hitMouse.point.z);
+            mouseCursor.transform.position = posWithoutY;
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            Vector3 offsetY = new Vector3(0.0f, godSphereSeedOnY, 0.0f);
+            mouseCursor.transform.position += offsetY;
+        } else if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (mouseCursor.transform.position.y > 0.0f) {
+                Vector3 offsetY = new Vector3(0.0f, godSphereSeedOnY, 0.0f);
+                mouseCursor.transform.position -= offsetY;
+            }
+        }
+
+        //Spawn de mob
+        if (coolDownTimer > 0.0f) { coolDownTimer -= Time.deltaTime; }
+
+        if (Input.GetMouseButtonDown(0) && coolDownTimer <= 0.0f)
         {
             Ray ray = sourceCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -34,8 +57,10 @@ public class RaycastSpawner : MonoBehaviour {
             {
                 GameObject.Instantiate(objectPrefab, hit.point, Quaternion.identity);
             }
+            coolDownTimer = coolDownBetweenMobSpawn;
         }
 
+        //Teleport du player
         if (Input.GetMouseButtonDown(1))
         {
             Ray ray = sourceCamera.ScreenPointToRay(Input.mousePosition);
---
+++ Assets/Scripts/SphereSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereSpawner : MonoBehaviour {
+
+    public GameObject sphere;
+    public int nbSphere = 5;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i=0; i<nbSphere; i++)
+        {
+            //random position
+            Vector3 pos = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(0.0f, 4.0f), Random.Range(-5.0f, 5.0f));
+            GameObject disSphere = (GameObject)Instantiate(sphere, pos, Quaternion.identity);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
