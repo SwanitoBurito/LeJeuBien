3540c9fefe931743625accb678deb4e6209312df
Remi laptop <remi@uselessworks.com>
2016-05-22 21:10:17 +0200

    Message in ze skaille

---
+++ Assets/Scripts/VRMessageController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRMessageController : MonoBehaviour {
+
+    public GameObject head;
+    public float distance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = head.transform.position + (head.transform.forward * distance);
+        transform.rotation = Quaternion.LookRotation((transform.position - head.transform.position).normalized, head.transform.up);
+    }
+}
